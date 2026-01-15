2368b6253c1a19594fcf761a699998819654aa76
OleynikIlyaIgorevich <oleynikilyafond02@gmail.com>
2026-01-15 17:43:06 +0300

    all labs correct

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv)
 
 void init(void)
 {
-    glClearColor(1.0, 1.0, 0.8, 0.0); // Более светлый желтый фон
+    glClearColor(1.0, 1.0, 0.8, 0.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(-10.0, 10.0, -10.0, 10.0);
@@ -29,64 +29,59 @@ void draw(void)
     glClear(GL_COLOR_BUFFER_BIT);
     glViewport(10, 10, 1900, 1000);
 
-    // 1. Рисуем разноцветные точки разного размера
-    glPointSize(8.0); // Точка размером 8 пикселей
+    glPointSize(8.0);
     glBegin(GL_POINTS);
-    glColor3f(1.0, 0.0, 0.0); // Красная точка
+    glColor3f(1.0, 0.0, 0.0);
     glVertex2f(-9.0, 9.0);
 
-    glColor3f(0.0, 1.0, 0.0); // Зеленая точка
+    glColor3f(0.0, 1.0, 0.0);
     glVertex2f(-8.5, 9.0);
     glEnd();
 
-    glPointSize(12.0); // Точка размером 12 пикселей
+    glPointSize(12.0);
     glBegin(GL_POINTS);
-    glColor3f(0.0, 0.0, 1.0); // Синяя точка
+    glColor3f(0.0, 0.0, 1.0);
     glVertex2f(-8.0, 9.0);
 
-    glColor3f(1.0, 0.0, 1.0); // Фиолетовая точка
+    glColor3f(1.0, 0.0, 1.0);
     glVertex2f(-7.5, 9.0);
     glEnd();
 
-    glPointSize(16.0); // Точка размером 16 пикселей
+    glPointSize(16.0);
     glBegin(GL_POINTS);
-    glColor3f(1.0, 1.0, 0.0); // Желтая точка
+    glColor3f(1.0, 1.0, 0.0);
     glVertex2f(-7.0, 9.0);
 
-    glColor3f(0.0, 1.0, 1.0); // Голубая точка
+    glColor3f(0.0, 1.0, 1.0);
     glVertex2f(-6.5, 9.0);
     glEnd();
 
-    // 2. Горизонтальная линия, разделяющая экран пополам
-    glLineWidth(3.0); // Толщина линии 3 пикселя
+    glLineWidth(3.0);
     glBegin(GL_LINES);
-    glColor3f(0.0, 0.0, 0.0); // Черный цвет
-    glVertex2f(-10.0, 0.0);   // Начало линии
-    glVertex2f(10.0, 0.0);    // Конец линии
+    glColor3f(0.0, 0.0, 0.0);
+    glVertex2f(-10.0, 0.0);
+    glVertex2f(10.0, 0.0);
     glEnd();
 
-    // 3. ЗАКРАШЕННЫЕ ПРИМИТИВЫ (выше линии y=0)
-
-    // а) Закрашенный треугольник (GL_TRIANGLES)
     glBegin(GL_TRIANGLES);
-    glColor3f(1.0, 0.0, 0.0); // Красный
+    glColor3f(1.0, 0.0, 0.0);  
     glVertex2f(-9.0, 8.0);
+    glColor3f(0.0, 1.0, 0.0);  
     glVertex2f(-7.0, 8.0);
+    glColor3f(0.0, 0.0, 1.0); 
     glVertex2f(-8.0, 6.0);
     glEnd();
 
-    // б) Закрашенный четырехугольник (GL_QUADS)
     glBegin(GL_QUADS);
-    glColor3f(0.0, 1.0, 0.0); // Зеленый
+    glColor3f(0.0, 1.0, 0.0);
     glVertex2f(-6.0, 8.0);
     glVertex2f(-4.0, 8.0);
     glVertex2f(-4.0, 6.0);
     glVertex2f(-6.0, 6.0);
     glEnd();
 
-    // в) Закрашенный многоугольник (GL_POLYGON) - пятиугольник
     glBegin(GL_POLYGON);
-    glColor3f(0.0, 0.0, 1.0); // Синий
+    glColor3f(0.0, 0.0, 1.0);
     glVertex2f(-3.0, 8.0);
     glVertex2f(-1.5, 8.0);
     glVertex2f(-1.0, 7.0);
@@ -94,9 +89,8 @@ void draw(void)
     glVertex2f(-3.5, 6.0);
     glEnd();
 
-    // г) Закрашенные треугольники в виде полосы (GL_TRIANGLE_STRIP)
     glBegin(GL_TRIANGLE_STRIP);
-    glColor3f(1.0, 0.0, 1.0); // Фиолетовый
+    glColor3f(1.0, 0.0, 1.0);
     glVertex2f(1.0, 8.0);
     glVertex2f(2.0, 8.0);
     glVertex2f(1.5, 7.0);
@@ -104,22 +98,18 @@ void draw(void)
     glVertex2f(2.5, 6.0);
     glEnd();
 
-    // 4. ПРОВОЛОЧНЫЕ ПРИМИТИВЫ (ниже линии y=0)
+    glLineWidth(2.0);
 
-    glLineWidth(2.0); // Толщина линий 2 пикселя
-
-    // а) Отдельные линии (GL_LINES)
     glBegin(GL_LINES);
-    glColor3f(1.0, 0.5, 0.0); // Оранжевый
+    glColor3f(1.0, 0.5, 0.0);
     glVertex2f(-9.0, -2.0);
     glVertex2f(-7.0, -2.0);
     glVertex2f(-8.0, -4.0);
     glVertex2f(-6.0, -4.0);
     glEnd();
 
-    // б) Ломаная линия (GL_LINE_STRIP)
     glBegin(GL_LINE_STRIP);
-    glColor3f(0.5, 0.0, 0.5); // Пурпурный
+    glColor3f(0.5, 0.0, 0.5);
     glVertex2f(-5.0, -2.0);
     glVertex2f(-4.0, -4.0);
     glVertex2f(-3.0, -2.0);
@@ -127,17 +117,15 @@ void draw(void)
     glVertex2f(-1.0, -2.0);
     glEnd();
 
-    // в) Замкнутая ломаная линия (GL_LINE_LOOP) - треугольник
     glBegin(GL_LINE_LOOP);
-    glColor3f(0.0, 0.5, 0.5); // Бирюзовый
+    glColor3f(0.0, 0.5, 0.5);
     glVertex2f(1.0, -2.0);
     glVertex2f(3.0, -2.0);
     glVertex2f(2.0, -4.0);
     glEnd();
 
-    // г) Прямоугольник из линий (используем GL_LINE_LOOP)
     glBegin(GL_LINE_LOOP);
-    glColor3f(0.5, 0.5, 0.0); // Оливковый
+    glColor3f(0.5, 0.5, 0.0);
     glVertex2f(4.0, -2.0);
     glVertex2f(6.0, -2.0);
     glVertex2f(6.0, -4.0);
---
+++ Lab3/Lab3.cpp
@@ -0,0 +1,156 @@
+﻿#include <GL/glut.h>
+
+float angle = 0;
+float cubeX = 0.0f;
+float cubeSpeed = 0.0005f;
+int cubeDirection = 1; 
+
+void init(void);
+void draw(void);
+void drawCube(float x, float y, float z, float size);
+void drawSphere(float x, float y, float z, float radius);
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(1280, 1024);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("3D Объекты");
+
+    init();
+    glutDisplayFunc(draw);
+    glutIdleFunc(draw); 
+    glutMainLoop();
+    return 0;
+}
+
+void init(void)
+{
+    glClearColor(0.2, 0.2, 0.3, 0.0);
+    glEnable(GL_DEPTH_TEST);
+
+    // Включаем освещение
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+
+    // Чтобы glColor3f влиял на материал
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+
+    // Настройка проекции
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-6.0, 6.0, -6.0, 6.0, -10.0, 10.0);
+
+    // === КРИТИЧЕСКИ ВАЖНО: переключиться на модельно-видовую матрицу и сбросить её ===
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // Теперь устанавливаем позицию света — она будет в мировых координатах!
+    GLfloat light_ambient[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+    GLfloat light_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    GLfloat light_position[] = { 2.0f, 2.0f, 3.0f, 1.0f }; // точечный источник
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+}
+
+void drawCube(float x, float y, float z, float size)
+{
+    glPushMatrix();
+
+    glTranslatef(x, y, z);
+    glRotatef(angle, 1.0, 1.0, 0.0);
+
+    glutSolidCube(size);
+
+    glPopMatrix();
+}
+
+void drawSphere(float x, float y, float z, float radius)
+{
+    glPushMatrix();
+
+    glTranslatef(x, y, z);
+    glRotatef(angle, 1.0, 1.0, 0.0);
+
+    glutSolidSphere(radius, 50, 50);
+
+    glPopMatrix();
+}
+
+void draw(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glViewport(0, 0, 1280, 1024);
+
+    angle += 0.1;
+    if (angle > 360) angle = 0;
+
+    cubeX += cubeSpeed * cubeDirection;
+
+    if (cubeX > 2.0f) {
+        cubeX = 2.0f;
+        cubeDirection = -1; 
+    }
+    else if (cubeX < -2.0f) {
+        cubeX = -2.0f;
+        cubeDirection = 1;
+    }
+
+    GLfloat mat_shininess = 50.0;
+
+    drawSphere(-2.5, 2.5, 0.0, 0.8);
+
+    glPushMatrix();
+
+    glTranslatef(2.5, 2.5, 0.0);
+    glRotatef(angle, 0.0, 1.0, 1.0);
+    glutWireCube(1.0);
+    glPopMatrix();
+
+    glPushMatrix();
+
+    glMaterialf(GL_FRONT, GL_SHININESS, mat_shininess);
+    glTranslatef(-2.5, -2.5, 0.0);
+    glRotatef(angle, 1.0, 0.0, 0.0);
+    glutSolidTorus(0.2, 0.8, 30, 30);
+    glPopMatrix();
+
+    glPushMatrix();
+
+    glMaterialf(GL_FRONT, GL_SHININESS, mat_shininess);
+    glTranslatef(2.5, -2.5, 0.0);
+    glRotatef(angle, 0.0, 0.0, 1.0);
+    glutWireTorus(0.2, 0.8, 20, 20);
+    glPopMatrix();
+
+    drawCube(cubeX, 0.0, 0.0, 1.0);
+
+    // Визуализация положения источников света (опционально)
+    glPushMatrix();
+    glDisable(GL_LIGHTING);
+    glColor3f(1.0f, 1.0f, 0.0f); // Желтый для источников света
+    glPointSize(10.0f);
+    glBegin(GL_POINTS);
+    // Первый источник света
+    glVertex3f(3.0f, 5.0f, 3.0f);
+    glVertex3f(-3.0f, 5.0f, -3.0f);
+    glEnd();
+    glEnable(GL_LIGHTING);
+    glPopMatrix();
+
+    glPushMatrix();
+    glDisable(GL_LIGHTING);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glRasterPos2f(-3.8f, -3.8f);
+
+    glEnable(GL_LIGHTING);
+    glPopMatrix();
+
+    glutSwapBuffers();
+}
